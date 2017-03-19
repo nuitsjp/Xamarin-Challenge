e0dcce041d7a19608c12054bc1f02cc549c5a054
nuits.jp <nuits.jp@live.jp>
2017-03-19 09:43:58 +0900

    Add part1 contents.

---
+++ Part1/Resources/Portable/Helpers/WeatherHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolBreeze.Helpers
+{
+    public class WeatherHelper
+    {
+        public async static Task<WeatherInformation> GetCurrentConditionsAsync(string cityName, string countryCode)
+        {
+            string url = $"http://traininglabservices.azurewebsites.net/api/weather/current/city?cityName={cityName}&countryCode={countryCode}";
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetStringAsync(url);
+
+            var result = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherInformation>(response);
+
+            return result;
+        }
+    }
+}
